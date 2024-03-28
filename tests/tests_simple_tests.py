8ff2c62f197a4f19f77264f19f7d908a0bb7b82e
Alex Ov <alexov@swisscows.email>
2024-03-28 20:54:25 +0200

    Add test test_magento_stb_search()

--- tests/tests_simple_tests.py
+++ tests/tests_simple_tests.py
@@ -1,4 +1,4 @@
-from selene import browser, have
+from selene import browser, have, be
 
 
 def test_the_internent_heroku_login(get_browser):
@@ -8,3 +8,12 @@ def test_the_internent_heroku_login(get_browser):
     browser.element("button[type='submit']").click()
 
     browser.element("h2").should(have.text("Secure Area"))
+
+
+def test_magento_stb_search(get_browser):
+    browser.open('https://magento.softwaretestingboard.com/')
+    browser.element("#search").type("pants")
+    browser.element("#search").press_enter()
+    browser.element(".product-item-link").should(be.visible)
+
+    browser.element(".product-item-link").should(have.text("Pant"))
