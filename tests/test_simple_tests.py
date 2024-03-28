83f83a8dda69476649c5992e38b589a36d30f2e4
Alex Ov <alexov@swisscows.email>
2024-03-28 22:02:53 +0200

    Add allure steps

--- tests/test_simple_tests.py
+++ tests/test_simple_tests.py
@@ -1,19 +1,24 @@
+import allure
 from selene import browser, have, be
 
 
 def test_the_internent_heroku_login(get_browser):
-    browser.open('https://the-internet.herokuapp.com/login')
-    browser.element("#username").type("tomsmith")
-    browser.element("#password").type("SuperSecretPassword!")
-    browser.element("button[type='submit']").click()
-
-    browser.element("h2").should(have.text("Secure Area"))
+    with allure.step("Открыть страницу логина на the-internet.herokuapp и залогиниться"):
+        browser.open('https://the-internet.herokuapp.com/login')
+        browser.element("#username").type("tomsmith")
+        browser.element("#password").type("SuperSecretPassword!")
+        browser.element("button[type='submit']").click()
+    with allure.step("Проверить что залогинились"):
+        browser.element("h2").should(have.text("Secure Area"))
 
 
 def test_magento_stb_search(get_browser):
-    browser.open('https://magento.softwaretestingboard.com/')
-    browser.element("#search").type("pants")
-    browser.element("#search").press_enter()
-    browser.element(".product-item-link").should(be.visible)
+    with allure.step("Открыть страницу Magento STB"):
+        browser.open('https://magento.softwaretestingboard.com/')
+    with allure.step("Сделать поиск 'pants'"):
+        browser.element("#search").type("pants")
+        browser.element("#search").press_enter()
 
-    browser.element(".product-item-link").should(have.text("Pant"))
+    with allure.step("Проверить наличие Pant в поисковой выдаче"):
+        browser.element(".product-item-link").should(be.visible)
+        browser.element(".product-item-link").should(have.text("Pant"))
